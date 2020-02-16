896d97b07da00bb79f9b4965dd86c4f408ded9f5
Nyelson Gomes Barbosa <nyelsonbarbosa@hotmail.com>
2020-02-16 20:21:47 -0300

    Iniciando a base da aplicação

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-//teste 2
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+mongoose.connect('mongodb+srv://admin:admin@tcc-database-ftyyo.mongodb.net/test?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+app.use(express.json());
+
+app.get('/users', (request, response) => {
+    console.log(request.query);
+
+    return response.json({
+        message: 'Hello!'
+    });
+});
+
+app.delete('/users/:id', (request, response) => {
+    console.log(request.params);
+
+    return response.json({
+        message: 'Hello!'
+    });
+});
+
+app.post('/users', (request, response) => {
+    console.log(request.body);
+
+    return response.json({
+        message: 'Hello!'
+    });
+});
+
+app.listen(3333);
