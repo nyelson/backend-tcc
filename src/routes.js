748ad865dd8a9ec62b0e2e03081c5c498b4aa785
Nyelson Gomes Barbosa <nyelsonbarbosa@hotmail.com>
2020-02-26 20:33:24 -0300

    Adicionando os ids nas entidades para evitar um id gerado automaticamente

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,8 @@
 const { Router } = require('express');
 const Usuario = require('./models/Usuario');
-// const Time = require('./models/Time');
-// const Item = require('./models/Item');
+const Time = require('./models/Time');
+const Item = require('./models/Item');
+const mongoose = require('mongoose');
 
 const routes = Router();
 
@@ -21,18 +22,34 @@ routes.delete('/users/:id', (request, response) => {
    });
 });
 
-routes.post('/usuarios', async (request, response) => {
+routes.post('/usuarios', async (request, response, {usuarioID, timeID}) => {
    const { nome, cargo, tecnologias, times } = request.body;
-   // const timesArray = times.split(',').map(time => time.Time());
+
    const tecnologiasArray = tecnologias
       .split(',')
       .map(tecnologia => tecnologia.trim());
 
-   const usuario = await Usuario.create({
+   let usuario = await Usuario.create({
       nome,
       cargo,
       tecnologias: tecnologiasArray,
-      times,
+   });
+
+   times.forEach(async element => {
+      let updateTimes = await Time.create(
+         { 
+            nome: element.nome,
+         }
+      )
+
+      // TO-DO verificar existencia de time antes de adicionar na base
+      let teste1 = await Time.updateOne({_id: updateTimes._id}, { "$addToSet": {usuarios : usuario._id} }, (res) => {
+         console.log(res);
+      });
+
+      let teste2 = await Usuario.updateOne({_id: usuario._id}, { "$addToSet": {times : updateTimes._id} }, (res) => {
+         console.log(res);
+      });
    });
 
    return response.json(usuario);
