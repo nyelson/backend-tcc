6fd70fd3ce4fd523dd6d486e61aee8520d4e3ab2
Nyelson Gomes Barbosa <nyelsonbarbosa@hotmail.com>
2020-03-01 18:35:46 -0300

    Alterações na usuario controller apos ter rodado o lint:fix

--- usuario-services/src/routes.js
+++ usuario-services/src/routes.js
@@ -1,12 +1,10 @@
 const { Router } = require('express');
 const UsuarioController = require('./controllers/UsuarioController');
-const Usuario = require('./models/Usuario');
-const Time = require('./models/Time');
 
 const routes = Router();
 
 routes.post('/usuarios', UsuarioController.addUser);
-routes.post('/usuarios/:user_id', UsuarioController.findUser);
-routes.delete('/usuarios/delete/:user_id', UsuarioController.deleteUser);
+routes.post('/usuarios/:id', UsuarioController.findUser);
+routes.delete('/usuarios/delete/:id', UsuarioController.deleteUser);
 
 module.exports = routes;
