78c2772157f36f2bd6792b81dd3fadafab8ff6e8
Ramon Portela Santos <portela.ramon96@gmail.com>
2020-03-01 18:20:33 -0300

    Criação do serviço resposável pelos times (#2)
    Co-authored-by: Nyelson Gomes Barbosa <nyelsonbarbosa@hotmail.com>

---
+++ teams-service/src/routes/routes.js
@@ -0,0 +1,27 @@
+const { Router } = require('express');
+const Time = require('../models/Time');
+
+const routes = Router();
+
+const validateTeam = (request, response, next) => {
+   const { nome } = request.body;
+
+   if (!nome)
+      return response
+         .status(400)
+         .json({ erro: 'Campos obrigatórios não preenchidos' });
+
+   return next();
+};
+
+routes.post('/times', validateTeam, async (request, response) => {
+   const { nome } = request.body;
+
+   const time = await Time.create({
+      nome,
+   });
+
+   return response.json(time);
+});
+
+module.exports = routes;
