bb37ab738f9ddbbb9101303544d59503caf2c532
ramon portela <portela.ramon96@gmail.com>
2020-04-26 20:34:19 -0300

    Work in Progress
    Sign up
    Sign in
    Sign out

---
+++ front-end/src/components/core/AppBar.vue
@@ -0,0 +1,88 @@
+<template>
+  <v-app-bar id="app-bar" absolute app color="transparent" flat height="75">
+    <v-toolbar-title>{{ $route.name }}</v-toolbar-title>
+    <v-btn
+      v-for="item in itens"
+      :key="item.name"
+      :to="item.href"
+      class="ml-2"
+      min-width="0"
+      text
+    >
+      <v-icon>{{ item.icon }}</v-icon>
+    </v-btn>
+
+    <v-spacer />
+
+    <v-menu
+      bottom
+      left
+      offset-y
+      origin="top right"
+      transition="scale-transition"
+    >
+      <template v-slot:activator="{ attrs, on }">
+        <v-btn class="ml-2" min-width="0" text v-bind="attrs" v-on="on">
+          <v-icon>mdi-account</v-icon>
+        </v-btn>
+      </template>
+      <v-list :tile="false" nav>
+        <v-hover v-slot:default="{ hover }">
+          <v-list-item
+            :dark="hover"
+            :link="true"
+            activeClass=""
+            :class="{
+              'black--text': !hover,
+              'white--text secondary elevation-12': hover,
+            }"
+          >
+            <v-list-item-content>Perfil</v-list-item-content>
+          </v-list-item>
+        </v-hover>
+        <v-hover v-slot:default="{ hover }">
+          <v-list-item
+            :dark="hover"
+            :link="true"
+            @click="logout"
+            activeClass=""
+            :class="{
+              'black--text': !hover,
+              'white--text secondary elevation-12': hover,
+            }"
+          >
+            <v-list-item-content>Logout</v-list-item-content>
+            <v-list-item-icon>
+              <v-icon>mdi-logout</v-icon>
+            </v-list-item-icon>
+          </v-list-item>
+        </v-hover>
+      </v-list>
+    </v-menu>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: "DashboardCoreAppBar",
+  data: () => ({
+    itens: [
+      {
+        icon: "mdi-clipboard-outline",
+        name: "Itens",
+        href: "/dashboard/tracker",
+      },
+      {
+        icon: "mdi-view-dashboard",
+        name: "Dashboard",
+        href: "/dashboard/dashboard",
+      },
+    ],
+  }),
+  methods: {
+    logout() {
+      this.$store.dispatch("logout");
+    },
+  },
+};
+</script>
---
+++ front-end/src/components/core/Drawer.vue
@@ -0,0 +1,57 @@
+<template>
+  <v-navigation-drawer id="drawer" v-model="drawer" dark app mobile-break-point="960" width="260">
+    <v-list>
+      <v-list-item>
+        <v-list-item-avatar class="align-self-center" color="blue" contain>
+          {{
+          projectInitials
+          }}
+        </v-list-item-avatar>
+
+        <v-list-item-content>
+          <v-list-item-title class="display-1" v-text="projectName" />
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+
+    <v-divider class="mb-2" />
+
+    <v-list expand dense nav>
+      <v-list-item v-for="item in itens" :key="item.name" :to="item.href" link>
+        <v-list-item-icon>
+          <v-icon v-if="item.icon">{{ item.icon }}</v-icon>
+        </v-list-item-icon>
+        <v-list-item-content>{{ item.name }}</v-list-item-content>
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+<script>
+export default {
+  name: "DashboardCoreDrawer",
+  data: () => ({
+    projectName: "Projeto X",
+    drawer: true,
+    itens: [
+      {
+        icon: "mdi-clipboard-outline",
+        name: "Itens",
+        href: "/dashboard/tracker"
+      },
+      {
+        icon: "mdi-view-dashboard",
+        name: "Dashboard",
+        href: "/dashboard/dashboard"
+      }
+    ]
+  }),
+  computed: {
+    projectInitials() {
+      return this.projectName
+        .split(" ")
+        .map(word => word.substring(0, 1))
+        .join("");
+    }
+  }
+};
+</script>
