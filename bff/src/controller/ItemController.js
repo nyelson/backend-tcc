3d7735f5cc6a403e9b096769e3518ef150733d7b
ramon portela <portela.ramon96@gmail.com>
2020-05-01 22:05:31 -0300

    Add pagination to fetchItems

--- bff/src/controller/ItemController.js
+++ bff/src/controller/ItemController.js
@@ -1,16 +1,34 @@
 const axios = require('axios');
 
+const fetchItems = (teams, page, itemsPerPage) =>
+   axios.get(
+      `http://localhost:3331/items/teams?page=${page}&itemsPerPage=${itemsPerPage}&${teams
+         .map((team) => `teamsIds=${team}`)
+         .join('&')}`
+   );
+
+const fetchTotalRecords = (teams) =>
+   axios.get(
+      `http://localhost:3331/items/teams/count?${teams
+         .map((team) => `teamsIds=${team}`)
+         .join('&')}`
+   );
 const findItemsByUser = async (req, res) => {
    const { times: teams } = req.user;
+   const { page, itemsPerPage } = req.query;
 
    try {
-      const resp = await axios.get(
-         `http://localhost:3331/items/teams?${teams
-            .map((team) => `teamsIds=${team}`)
-            .join('&')}`
-      );
+      const [
+         { data: items },
+         {
+            data: { totalRecords },
+         },
+      ] = await axios.all([
+         fetchItems(teams, page, itemsPerPage),
+         fetchTotalRecords(teams),
+      ]);
 
-      return res.status(resp.status).json(resp.data);
+      return res.status(200).json({ items, totalRecords });
    } catch (err) {
       if (err.response) {
          return res.status(err.response.status).json(err.response.data);
